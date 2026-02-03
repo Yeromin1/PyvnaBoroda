79667720e81a6d97317598a3e70dc87e725ccd2d
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-02-03 20:30:40 +0200

    key-id

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,35 @@ import Product from "./Product.jsx";
 export default function App() {
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Best selling</h1>
 
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
     </div>
   );
 }
+
+// import BookList from "./Product.jsx";
+
+// const favBooks = [
+//   { id: "id-1", name: "JS for beginners" },
+//   { id: "id-2", name: "React basics" },
+//   { id: "id-3", name: "React Router overview" },
+// ];
+
+// export default function App() {
+//   return (
+//     <>
+//       <h1>Books of the week</h1>
+//       <BookList books={favBooks} />
+//     </>
+//   );
+// }
